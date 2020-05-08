76b3b20959927b24aa54d0e7cec522e1dc35f7d6
Piotr Łępa <piotr.lepa@altconnect.pl>
2020-05-08 15:16:45 +0200

    create business, dao and rest files

---
+++ api/REST/post.endpoint.js
@@ -0,0 +1,15 @@
+'use strict';
+import business from '../business/business.container';
+
+const postEndpoint = (router) => {
+  router.get('/api/posts', async (request, response, next) => {
+    try {
+      let result = await
+        business(request).getPostManager().query();
+      response.status(200).send(result);
+    } catch (error) {
+      console.log(error);
+    }
+  });
+};
+export default postEndpoint;
