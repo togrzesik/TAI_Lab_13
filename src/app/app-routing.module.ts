f9430f07ba522d1f5e6e8089c4481ec16a4bad6d
Piotr Łępa <piotr.lepa@altconnect.pl>
2020-05-08 15:33:59 +0200

    create add-post page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import {HomeComponent} from "./components/home/home.component";
 import {ContactComponent} from "./components/contact/contact.component";
 import {BlogItemDetailsComponent} from "./components/blog-item-details/blog-item-details.component";
 import {BlogHomeComponent} from "./components/blog-home/blog-home.component";
+import {AddPostComponent} from "./components/add-post/add-post.component";
 
 
 const routes: Routes = [
@@ -22,6 +23,10 @@ const routes: Routes = [
     path: 'blog/detail/:id',
     component: BlogItemDetailsComponent,
   },
+  {
+    path: 'add-post',
+    component: AddPostComponent,
+  }
 ];
 
 
